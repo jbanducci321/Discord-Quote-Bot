c35ee401bac2578453635eacd3dfa421bb52d78f
Jacob Banducci <jbanducci321@gmail.com>
2026-04-20 14:51:07 -0700

    Updated db table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS quote_bot_quotes (
+DROP TABLE IF EXISTS quote_bot_quotes;
+
+CREATE TABLE quote_bot_quotes (
     id INT NOT NULL AUTO_INCREMENT,
     quote_text TEXT NOT NULL,
     quoted_person VARCHAR(100) NOT NULL,
-    category VARCHAR(100) NOT NULL,
     added_by_user_id VARCHAR(30) NOT NULL,
     added_by_username VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,

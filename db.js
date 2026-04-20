2ad7833d8383dc91281392b71c50fce3c22e6815
Jacob Banducci <jbanducci321@gmail.com>
2026-04-20 15:27:50 -0700

    Small updates to the bot

--- db.js
+++ db.js
@@ -1,22 +1,24 @@
 import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-    host: "k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: "nm2pf20notjcum1m",
-    connectionLimit: 10,
-    waitForConnections: true
-});
-
+//Jacob
 // const pool = mysql.createPool({
-//     host: "jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     host: "k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 //     user: process.env.DB_USERNAME,
 //     password: process.env.DB_PASSWORD,
-//     database: "u2d8f0jswasdnehx",
-//     connectionLimit: 8,
+//     database: "nm2pf20notjcum1m",
+//     connectionLimit: 10,
 //     waitForConnections: true
 // });
+
+//Daniel
+const pool = mysql.createPool({
+    host: "jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "u2d8f0jswasdnehx",
+    connectionLimit: 8,
+    waitForConnections: true
+});
 
 export default pool;
